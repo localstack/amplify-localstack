b25edf11a477484dd88a69d196e48bf8f8cb32c3
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2023-03-24 12:36:24 -0500

    use-localstack parameter

--- event-handlers/handle-PreInit.js
+++ event-handlers/handle-PreInit.js
@@ -3,11 +3,25 @@ const yesno = require('yesno');
 const patches = require("../patches")
 
 async function run(context, args) {
-  // console.log("preinit")
-  const doPatch = await yesno({
-      question: 'Do you want to create the project in LocalStack? [y/N]',
-      defaultValue: "n"
-  });
+  const options = context.parameters.options
+  let doPatch = false
+  const value = options['use-localstack']
+  if (value != undefined){
+    if (value == 'true'){
+      doPatch = true
+    }else if(value == 'false'){
+      doPatch = false
+    }else{
+      context.print.error(`ERROR: "${value}" is an invalid value for parameter --use-localstack. Please use true or false`)
+    }
+
+  }else{
+    const doPatch = await yesno({
+        question: 'Do you want to create the project in LocalStack? [y/N]',
+        defaultValue: "n"
+    });
+  }
+
   if (doPatch == true){
     patches.patchEverything()
   }

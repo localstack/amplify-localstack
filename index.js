a6ca09cbf5d89d1774ee7830283feeb5c78c8b12
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2023-04-21 12:33:52 -0500

    eslint addition

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const path = require('path');
+const path = require('path')
 
-function init(context) {
+function init (context) {
 }
 
-function initEnv(context, providerMetadata) {
+function initEnv (context, providerMetadata) {
 }
 
-function onInitSuccessful(context) {}
+function onInitSuccessful (context) {}
 
-function pushResources(context, resourceList) {}
+function pushResources (context, resourceList) {}
 
-function deleteEnv(context, envName, deleteS3) {}
+function deleteEnv (context, envName, deleteS3) {}
 
-function configure(context) {}
+function configure (context) {}
 
-async function executeAmplifyCommand(context) {
-  const commandsDirPath = path.normalize(path.join(__dirname, 'commands'));
-  const commandPath = path.join(commandsDirPath, context.input.command);
-  const commandModule = require(commandPath);
-  await commandModule.run(context);
+async function executeAmplifyCommand (context) {
+  const commandsDirPath = path.normalize(path.join(__dirname, 'commands'))
+  const commandPath = path.join(commandsDirPath, context.input.command)
+  const commandModule = require(commandPath)
+  await commandModule.run(context)
 }
 
-async function handleAmplifyEvent(context, args) {
-  const eventHandlersDirPath = path.normalize(path.join(__dirname, 'event-handlers'));
-  const eventHandlerPath = path.join(eventHandlersDirPath, `handle-${args.event}`);
-  const eventHandlerModule = require(eventHandlerPath);
-  await eventHandlerModule.run(context, args);
+async function handleAmplifyEvent (context, args) {
+  const eventHandlersDirPath = path.normalize(path.join(__dirname, 'event-handlers'))
+  const eventHandlerPath = path.join(eventHandlersDirPath, `handle-${args.event}`)
+  const eventHandlerModule = require(eventHandlerPath)
+  await eventHandlerModule.run(context, args)
 }
 
 module.exports = {
@@ -36,5 +36,5 @@ module.exports = {
   configure,
   pushResources,
   executeAmplifyCommand,
-  handleAmplifyEvent,
-};
+  handleAmplifyEvent
+}
